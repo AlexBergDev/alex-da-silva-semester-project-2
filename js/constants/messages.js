58ba38e8cb98e54627cbb7fc710a94ef595d2172
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-02 01:10:54 +0100

    Create messages.js

---
+++ js/constants/messages.js
@@ -0,0 +1,9 @@
+export const EMPTY_INPUT_VALUE = "Please fill out all the inputs";
+export const ARTICLE_PUBLISHED = "Article published";
+export const EMPTY_ARTICLES = "No articles found";
+export const EMPTY_BOOKMARKS = "No bookmarks yet";
+export const SAVED_SUCCESS = "Changes successfully saved";
+export const INVALID_LOGIN_VALUE = "Please enter valid login details";
+export const WRONG_LOGIN_VALUE = "Wrong email address or password";
+export const STANDARD_ERROR = "An error occured";
+export const DELETE_CONFIRM = "Are you sure? You won't be able to recover the article.";
