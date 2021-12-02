daf5a554e6122a601fa815f4247b5a6e2a1af907
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-02 01:10:50 +0100

    Create storage.js

---
+++ js/utils/storage.js
@@ -0,0 +1,56 @@
+const tokenKey = "token";
+const userKey = "user";
+
+export function saveToken(token) {
+    saveToStorage(tokenKey, token);
+}
+
+export function getToken() {
+    return getFromStorage(tokenKey);
+}
+
+export function saveUser(user) {
+    saveToStorage(userKey, user);
+}
+
+export function getUsername() {
+    const user = getFromStorage(userKey);
+
+    if (user) {
+        return user.username;
+    }
+
+    return null;
+}
+
+export function clearStorage() {
+    localStorage.clear();
+}
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromStorage(key) {
+    const value = localStorage.getItem(key);
+
+    if (!value) {
+        return null;
+    }
+
+    return JSON.parse(value);
+}
+
+export function getBookmarks() {
+    const favs = localStorage.getItem("bookmarks");
+
+    if (favs === null) {
+        return [];
+    } else {
+        return JSON.parse(favs);
+    }
+}
+
+export function saveBookmarks(favs) {
+    localStorage.setItem("bookmarks", JSON.stringify(favs));
+}
