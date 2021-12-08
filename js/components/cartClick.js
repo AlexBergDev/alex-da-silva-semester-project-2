b3bcb6e4c446961849ebc77fd4f1f63fe05ba529
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-08 19:03:48 +0100

    Create cartClick.js

---
+++ js/components/cartClick.js
@@ -0,0 +1,25 @@
+import { getCart } from "../utils/storage.js";
+import { saveCart } from "../utils/storage.js";
+
+export default function cartClick() {
+
+    const id = this.dataset.id;
+    const title = this.dataset.title;
+    const price = this.dataset.price;
+    const image = this.dataset.image;
+
+    const currentCartItems = getCart();
+
+    const cartItemExists = currentCartItems.find(function (cartItem) {
+        return cartItem.id === id;
+    });
+
+    if (cartItemExists === undefined) {
+        const cart = { id: id, title: title, price: price, image: image};
+        currentCartItems.push(cart);
+        saveCart(currentCartItems);
+    } else {
+        const newCartItems = currentCartItems.filter((cartItem) => cartItem.id !== id);
+        saveCart(newCartItems);
+    }
+}
