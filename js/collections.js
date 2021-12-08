3024ee06f4c80212a91bd2f3e455701d695a0d40
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-08 06:34:43 +0100

    Create collections.js

---
+++ js/collections.js
@@ -0,0 +1,24 @@
+import { baseUrl } from "./settings/api.js";
+import displayMessage from "./components/displayMessage.js";
+import navigation from "./components/navigation.js";
+import renderProducts from "./ui/renderProducts.js";
+import searchTitle from "./ui/searchTitle.js";
+
+const url = baseUrl + "products";
+
+// navigation();
+
+(async function () {
+    const container = document.querySelector(".collections-container")
+
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        renderProducts(json, container);
+        searchTitle(json, container);
+    } catch (error) {
+        console.log(error);
+        displayMessage("alert-danger", error, container);
+    }
+})();
