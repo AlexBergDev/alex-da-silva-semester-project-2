56b0a311c7395fe51310772640089d763050aaa8
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-08 10:49:22 +0100

    Update wishlist.js

--- js/wishlist.js
+++ js/wishlist.js
@@ -1,20 +1,20 @@
 import { EMPTY_BOOKMARKS } from "./constants/messages.js"
 import displayMessage from "./components/displayMessage.js";
-import navigation from "./components/navigation";
+import navigation from "./components/navigation.js";
 import { getBookmarks } from "./utils/storage.js";
 import bookmarkClick from "./components/bookmarkClick.js";
 
-navigation();
+// navigation();
 
 export default function renderBookmarks() {
-    const container = document.querySelector(".data");
+    const container = document.querySelector(".products-container");
 
     const bookmarks = getBookmarks();
 
     container.innerHTML = "";
 
     if (bookmarks.length === 0) {
-        displayMessage("alert-info", EMPTY_BOOKMARKS, ".data");
+        displayMessage("alert-info", EMPTY_BOOKMARKS, ".products-container");
     }
 
     for (let i = 0; i < bookmarks.length; i++) {
@@ -30,17 +30,20 @@ export default function renderBookmarks() {
             bookmarkClass = "far";
         }
         
-        container.innerHTML += `<div class="col-md-6 col-lg-3 my-3">
-                                            <div class="card shadow-sm border-0 rounded-3">
-                                                <div class="card-body">
-                                                    <h5 class="card-title text-truncate">${bookmarks[i].title}</h5>
-                                                    <p class="card-text text-truncate">${bookmarks[i].summary}</p>
-                                                    <p class="card-text">${bookmarks[i].author}</p>
-                                                    <a href="article.html?id=${bookmarks[i].id}" class="btn btn-primary">Read</a>
-                                                    <i class="nav-link fs-4 float-end ${bookmarkClass} fa-bookmark" data-id="${bookmarks[i].id}" data-title="${bookmarks[i].title}" data-summary="${bookmarks[i].summary}" data-author="${bookmarks[i].author}"></i>
-                                                </div>
-                                            </div>
-                                        </div>`;
+        container.innerHTML += `<div class="col">
+                                    <div class="card">
+                                        <a href="details.html?id=${bookmarks[i].id}">
+                                            <img src="${bookmarks[i].image}" class="card-img-top" alt="${bookmarks[i].title}">
+                                        </a>
+                                        <div class="card-body p-0">
+                                            <i class="card-icon float-end ${bookmarkClass} fa-heart" data-id="${bookmarks[i].id}" data-title="${bookmarks[i].title}" data-price="${bookmarks[i].price}" data-image="${bookmarks[i].image}"></i>
+                                            <a href="details.html?id=${bookmarks[i].id}">
+                                                <h3 class="m-0">${bookmarks[i].title}</h3>
+                                                <div class="card-text h3">${bookmarks[i].price} kr</div>
+                                            </a>     
+                                        </div>
+                                    </div>
+                                </div>`;
 
         const bookmarkButton = document.querySelectorAll(".card i");
 
