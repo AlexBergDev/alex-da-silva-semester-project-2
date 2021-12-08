8f9e30a885921e9192c6306b0d40f0c129042e8e
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-08 19:03:45 +0100

    Update details.js

--- js/details.js
+++ js/details.js
@@ -1,6 +1,11 @@
 import { baseUrl } from "./settings/api.js";
 import displayMessage from "./components/displayMessage.js";
 import navigation from "./components/navigation.js";
+import bookmarkClick from "./components/bookmarkClick.js";
+import { getBookmarks } from "./utils/storage.js";
+import cartClick from "./components/cartClick.js";
+
+const bookmarks = getBookmarks();
 
 // navigation();
 
@@ -9,6 +14,8 @@ const image = document.querySelector(".product-container_image");
 const breadcrumb = document.querySelector(".product-container_breadcrumb");
 const description = document.querySelector(".product-container_description");
 const price = document.querySelector(".product-container_price");
+const wishlist = document.querySelector(".product-container_wishlist");
+const addToCart = document.querySelector(".product-container_button");
 
 const queryString = document.location.search;
 
@@ -27,12 +34,36 @@ const productUrl = baseUrl + "/products/" + id;
         const response = await fetch(productUrl);
         const product = await response.json();
 
+        let bookmarkClass = "far";
+
+        const doesObjectExist = bookmarks.find(function (fav) {
+
+        return parseInt(fav.id) === product.id;
+        });
+
+        if (doesObjectExist) {
+            bookmarkClass = "fa";
+        }
+
         document.title = `Arctic Fashion | ${product.title}`;
         h2.innerHTML = `${product.title}`;
         image.innerHTML = `<img src="${baseUrl + product.image.url}" class="img-fluid" alt="${product.title}">`;
         breadcrumb.innerHTML = `${product.title}`;
         description.innerHTML = `${product.description}`;
         price.innerHTML = `${product.price} kr`;
+        wishlist.innerHTML = `<i class="${bookmarkClass} fa-heart" data-id="${product.id}" data-title="${product.title}" data-price="${product.price}" data-image="${baseUrl + product.image.url}"></i>`;
+        addToCart.innerHTML = `<button type="button" class="cart-button btn btn-dark shadow" data-id="${product.id}" data-title="${product.title}" data-price="${product.price}" data-image="${baseUrl + product.image.url}">Add to cart</button>`;
+
+        const bookmarkButton = document.querySelectorAll(".card-icon i");
+        const cartButton = document.querySelectorAll(".card-button");
+
+        bookmarkButton.forEach((button) => {
+            button.addEventListener("click", bookmarkClick);
+        });
+
+        cartButton.forEach((button) => {
+            button.addEventListener("click", cartClick);
+        });
 
     } catch (error) {
         displayMessage("alert-warning", error, ".product-container");
