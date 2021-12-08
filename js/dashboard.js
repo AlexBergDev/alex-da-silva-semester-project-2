9b062efdc2c2c490aa5fd412c4b52c2f50725bde
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-08 19:53:49 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,36 @@
 import logoutButton from "./components/logoutButton.js";
+import { baseUrl } from "./settings/api.js";
+import navigation from "./components/navigation.js";
+import displayMessage from "./components/displayMessage.js";
+import redirectUnauthorized from "./components/redirectUnauthorized.js";
 
 logoutButton();
+redirectUnauthorized();
+// navigation();
+
+const productUrl = baseUrl + "/products";
+
+(async function () {
+    const container = document.querySelector("tbody");
+    const loading = document.querySelector(".loading");
+
+    try {
+        const response = await fetch(productUrl);
+        const json = await response.json();
+
+        loading.innerHTML = "";
+        container.innerHTML = "";
+
+        json.forEach(function (product) {
+            container.innerHTML += `<tr>
+                                        <th scope="row">${product.id}</th>
+                                        <td>${product.title}</td>
+                                        <td>${product.price} kr</td>
+                                        <td><a href="edit.html?id=${product.id}"><i class="fs-6 fas fa-pen"></i></a></td>
+                                    </tr>`;
+        });
+
+    } catch (error) {
+        displayMessage("alert-danger", error, "tbody");
+    }
+})();
