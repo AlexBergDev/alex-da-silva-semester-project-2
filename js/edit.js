cbc07f434530a135b8152fb0c6fe65a7917f9303
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-12 16:42:12 +0100

    Create edit.js

---
+++ js/edit.js
@@ -0,0 +1,111 @@
+import { EMPTY_INPUT_VALUE, SAVED_SUCCESS } from "./constants/messages.js"
+import navigation from "./components/navigation.js";
+import redirectUnauthorized from "./components/redirectUnauthorized.js";
+import { baseUrl } from "./settings/api.js";
+import displayMessage from "./components/displayMessage.js";
+import { getToken } from "./utils/storage.js";
+import deleteButton from "./components/deleteButton.js";
+
+const token = getToken();
+
+redirectUnauthorized();
+// navigation();
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+if (!id) {
+    document.location.href = "/dashboard.html";
+}
+
+const productUrl = baseUrl + "/products/" + id;
+
+const form = document.querySelector("form");
+const idInput = document.querySelector("#product-id");
+const title = document.querySelector("#product-title");
+const description = document.querySelector("#product-description");
+const price = document.querySelector("#product-price");
+const featured = document.querySelector("#product-featured");
+const image_url = document.querySelector("#product-image_url");
+const message = document.querySelector(".message-container");
+
+(async function () {
+    try {
+        const response = await fetch(productUrl);
+        const details = await response.json();
+
+        title.value = details.title;
+        description.value = details.description;
+        price.value = details.price;
+        idInput.value = details.id;
+        featured.value = details.featured;
+        image_url.value = details.image_url;
+
+        if (featured.value === "true"){
+            featured.checked = true;
+          } else {
+            featured.checked = false;
+          }
+
+        deleteButton(details.id);
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    message.innerHTML = "";
+
+    if (featured.checked == true){
+        featured.value = "true";
+      } else {
+        featured.value = "false";
+      }
+
+    const titleValue = title.value.trim();
+    const descriptionValue = description.value.trim();
+    const priceValue = price.value;
+    const idValue = idInput.value;
+    const featuredValue = featured.value;
+    const image_urlValue = image_url.value;
+
+    if (titleValue.length === 0 || descriptionValue.length === 0 || priceValue.length === 0) {
+        return displayMessage("alert-warning", EMPTY_INPUT_VALUE, ".message-container");
+    }
+
+    updateProduct(titleValue, descriptionValue, priceValue, idValue, featuredValue, image_urlValue);
+}
+
+async function updateProduct(title, description, price, id, featured, image_url) {
+    const url = baseUrl + "/products/" + id;
+    const data = JSON.stringify({ title: title, description: description, price: price, featured: featured, image_url: image_url});
+
+    const options = {
+        method: "PUT",
+        body: data,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const json = await response.json();
+
+        if (json.updated_at) {
+            displayMessage("alert-success", SAVED_SUCCESS, ".message-container");
+        }
+
+        if (json.error) {
+            displayMessage("alert-danger", json.message, ".message-container");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
