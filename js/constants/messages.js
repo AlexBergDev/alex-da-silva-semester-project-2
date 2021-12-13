706874d6ed53f62b10dc33e955453beac6eaaed8
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-12-13 01:37:38 +0100

    Update messages.js

--- js/constants/messages.js
+++ js/constants/messages.js
@@ -1,5 +1,5 @@
 export const EMPTY_INPUT_VALUE = "Please fill out all the inputs";
-export const ARTICLE_PUBLISHED = "Article published";
+export const PRODUCT_PUBLISHED = "Product published";
 export const EMPTY_PRODUCTS = "No products found";
 export const EMPTY_BOOKMARKS = "No bookmarks yet";
 export const EMPTY_CART = "No items in your cart yet";
@@ -7,4 +7,4 @@ export const SAVED_SUCCESS = "Changes successfully saved";
 export const INVALID_LOGIN_VALUE = "Please enter valid login details";
 export const WRONG_LOGIN_VALUE = "Wrong email address or password";
 export const STANDARD_ERROR = "An error occured";
-export const DELETE_CONFIRM = "Are you sure? You won't be able to recover the article.";
+export const DELETE_CONFIRM = "Are you sure? You won't be able to recover the product.";
